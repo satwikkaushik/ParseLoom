18d212d45991bfd2346753174d0a995b029c883e
satwikkaushik <satwikkaushik05@gmail.com>
2025-05-26 19:55:01 +0000

    Create API endpoint for LL1 Parser

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from LR0.LR0_Parser import LR0_Parser
+from LL1.LL1_Parser import LL1_Parser
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -25,7 +26,17 @@ def LR0_parser():
 
 @app.route("/LL1", methods=["POST"])
 def LL1_parser():
-    pass
+    input_data = request.get_json()
+    input_grammar = input_data.get("grammar")
+    input_string = input_data.get("string").split(" ")
+    
+    try:
+        LL1_parser = LL1_Parser(input_grammar, input_string)
+        result = LL1_parser.parse()
+        
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
